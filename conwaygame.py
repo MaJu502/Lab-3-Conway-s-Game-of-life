030ded0e7eaad28f3182e34b239f573f1a98f638
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-11-17 01:52:45 -0600

    fixes needed

--- conwaygame.py
+++ conwaygame.py
@@ -3,6 +3,7 @@ Universidad del valle
 Graficas por computadora
 @author Marco Jurado 20308
 """
+import math
 import time
 import pygame
 from OpenGL.GL import *
@@ -11,16 +12,15 @@ import numpy
 
 pygame.init()
 
-w,h = 500,500
+w,h = 800,600
 pantalla = pygame.display.set_mode( (w,h), pygame.OPENGL | pygame.DOUBLEBUF )
+red = (1,0,0)
+white = (1,1,1)
 
-#para generar celdas
-temp_w = w/5
-temp_h = h/5
-ceros_boundbox = (temp_w,temp_h)
-celdas = numpy.zeros(ceros_boundbox) #llena mapa de ceros
+
 
 def pixel(x, y, color):
+    print('holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa a   >   ',color)
     glEnable(GL_SCISSOR_TEST)
     glScissor(x, y, 9, 9)
     glClearColor(color[0], color[1], color[2], 1)
@@ -29,46 +29,73 @@ def pixel(x, y, color):
 
 #generar patron inicial
 coords_patron_inicial = [] #patron inicial
-
-for i in range(0, random.randint(1,10)):
+temp = random.randint(50,100)
+print('TEMP > ',temp)
+for i in range(0, temp):
     # Generacion de varios numberos random para la posicion de los puntos
-    cord_random_x = random.randint(1, 250)
-    cord_random_y = random.randint(1, 250)
+    cord_random_x = random.randint(200, 250)
+    cord_random_y = random.randint(200, 250)
     coords_patron_inicial.append( [cord_random_x, cord_random_y] )
 
 
 def Conway(celdas, size, updater_celdas):
-    celdas_muertas = numpy.zeros((celdas.numpy.shape[0], celdas.numpy.shape[1])) #indice de cada celda (x,y)
+    celdas_muertas = numpy.zeros((celdas.shape[0], celdas.shape[1])) #indice de cada celda (x,y)
     color = (1,0,0)
-    for x,y in numpy.ndindex(celdas[0], celdas[1]):
+
+    for x,y in numpy.ndindex(celdas.shape[0], celdas.shape[1]):
         #recorrer todas las celdas para verificar condiciones de juego
         celdas_vivas = numpy.sum( celdas[ x-1:x+2, y-1:y+2 ] ) - celdas[x,y] # slices de python para obtener celdas vivas (valor de x,y)
 
         # condiciones
         # si detecta que celdas en x,y son = 0 la celda se apaga  ---> o se pintan
+        if celdas[x,y] == 0: color = red
+        if celdas[x,y == 1]: color = white
 
         # si detecta que celdas en x,y = 1, si detecta que hay menos de dos celdas vivas vecinas se muere la celda
-        
+
         # si detecta que celdas en x,y = 1, si detecta que hay más de 3 celdas vivas vecinas se muere la celda
 
         # si detecta que celdas en x,y = 1, si detecta que hay menor o igual a 3 celdas vivas vecinas las celdas muertas en x,y se vuelven = 1, y las revivo
 
         # si detecta que celdas en x,y = 1, si detecta que hay menor o igual a 2 celdas vivas vecinas las celdas muertas en x,y se vuelven = 1, y las revivo
-
+        if celdas[x,y] == 1:
+            if celdas_vivas > 3 and updater_celdas == True: color = red
+            if celdas_vivas < 2 and updater_celdas == True: color = red
+            if celdas_vivas <= 3:
+                celdas_muertas[x,y] = 1
+                if updater_celdas == True: color = white
+            if celdas_vivas >= 2:
+                celdas_muertas[x,y] = 1
+                if updater_celdas == True: color = white
+            
         # else final si hay 3 celdas vivas, entonces las celulas muertas se vuelven = 1 en x,y, y las revivo
-
-
+        else:
+            if celdas_vivas == 3:
+                celdas_muertas[x,y] = 1
+                if updater_celdas == True: color = white
+            
+        #ahora hay que llamar a pixel para que pinte la celda
         #pintar las celdas que sean igual = 1 en x,y con funcion pixel
         pixel(x * size , y * size,  color)
 
         #devuelvo la recursion tratada
+        print('celdas > ', celdas_muertas)
         return celdas_muertas 
 
 #recorrer coordenadas para generar el patron inicial
+#para generar celdas
+temp_w = w
+temp_h = h
+ceros_boundbox = (round(temp_w),round(temp_h))
+celdas = numpy.zeros(ceros_boundbox) #llena mapa de ceros
+print('celda> > ', celdas.size)
+Conway(celdas, 10, False)
+pygame.display.flip()
 for i in coords_patron_inicial:
-    i[0]/5 #primer valor de celdas en x
-    i[1]/5 #segundo valor de celdas en y
-    celdas[i[0]/5, i[1]/5] = 1
+    i[0] #primer valor de celdas en x
+    i[1] #segundo valor de celdas en y
+    celdas[  round(i[0]), round(i[1])  ] = 1
+    print(' la nueva zelda > ',celdas[  round(i[0]), round(i[1])  ])
     Conway(celdas, 10, False)
 
 funcionar = True
@@ -83,4 +110,5 @@ while funcionar:
             
         if temp_updater == True:
             celdas = Conway(celdas, 10 , True)
-            time.sleep(2)
+            pygame.display.flip()
+            time.sleep(0.2)
